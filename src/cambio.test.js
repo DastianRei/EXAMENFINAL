ba5be83d0f420a4ee320d60befc77d526940e702
Dayan <dayan.aguilar@ucb.edu.bo>
2023-06-01 09:40:42 -0400

    arreglamos el github actions:

--- src/cambio.test.js
+++ src/cambio.test.js
@@ -1,5 +1,4 @@
-import cambiar from "./cambio";
-
+import {cambiar} from "./cambio";
 describe("Cambiar", () => {
     it("Deberia devolver el cambio", () => {
       expect(cambiar(10,15)).toEqual(5);
